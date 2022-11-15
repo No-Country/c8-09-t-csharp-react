bbe85f181211ae10a4d358eeee1f767c00a2e6db
Maximiliano <maximartins45@gmail.com>
2022-11-14 21:22:25 -0300

    configuracion de tailwind

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
-import {BrowserRouter} from "react-router-dom"
-import Nav from "./components/nav/Nav"
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import Nav from './components/nav/Nav'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-      <React.StrictMode>
-        <Nav />
-        <App/>
-      </React.StrictMode>
-    </BrowserRouter>
+	<BrowserRouter>
+		<React.StrictMode>
+			<Nav />
+			<App />
+		</React.StrictMode>
+	</BrowserRouter>
 )
