f0e677aca5f979dca6cca0cc30cfdb61bfb16161
nasanchez7 <nadirblanco02@gmail.com>
2022-11-16 17:33:52 -0300

    Haciendo seccion eventos reutilizable

--- Front/src/pages/home/Home.jsx
+++ Front/src/pages/home/Home.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import SeccionEvent from "../../components/nextEvents/SeccionEvent";
+
 
 const Home = () => {
+
+	const [events, setEvents] = useState([])
+
+    const eventsFetch = async () => {
+        const response = await fetch("https://635eb27203d2d4d47af47b8b.mockapi.io/Cohorte")
+        const data = await response.json()
+        setEvents([data[0], data[1], data[2]])
+    }
+
+    useEffect(()=> {
+        eventsFetch()
+    }, [])
+
+
 	return (
 		<div>
+			<SeccionEvent seccion={"Proximos eventos"} eventos={events}/>
 		</div>
 	)
 }
