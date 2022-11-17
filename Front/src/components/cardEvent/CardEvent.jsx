48d4fd1d3711cf686600dca43da32f4a7f2c6665
Maximiliano <maximartins45@gmail.com>
2022-11-17 13:07:56 -0300

    Solucionando problema de estilos

--- Front/src/components/cardEvent/CardEvent.jsx
+++ Front/src/components/cardEvent/CardEvent.jsx
@@ -1,42 +1,58 @@
-import "./cardEvent.css"
-import {Link} from "react-router-dom"
+import './cardEvent.css'
+import { Link } from 'react-router-dom'
 
-const CardEvent = ({evento}) => {
+const CardEvent = ({ evento }) => {
+	const meses = [
+		'',
+		'Ene',
+		'Feb',
+		'Mar',
+		'Abr',
+		'May',
+		'Jun',
+		'Jul',
+		'Ago',
+		'Sept',
+		'Oct',
+		'Nov',
+		'Dic',
+	]
+	const description = evento.description.slice(0, 20)
+	const fechaSinFormatear = evento.time.slice(0, 10)
+	const fecha = fechaSinFormatear.split('-')
+	const horario = evento.time.slice(11, 16)
 
-    const meses = ["", "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sept", "Oct", "Nov", "Dic"];
-    const description = evento.description.slice(0,20)
-    const fechaSinFormatear = evento.time.slice(0,10)
-    const fecha = fechaSinFormatear.split("-")
-    const horario = evento.time.slice(11, 16)
-
-    return(
-        <div className="cardEvento">
-            <img src={evento.image} alt={evento.title}/>
-            <div className="infoEvento">
-                <div className="containerInfo">
-                    <div className="fechaHoraContainer">
-                        <h3 className="fecha"> {fecha[2]} </h3>
-                        <div className="mesHora">
-                            <h3> {meses[fecha[1]]}, {fecha[0]}  </h3>
-                            <h4> {horario} </h4>
-                        </div>
-                    </div>
-                    <div className="eventoArtista">
-                        <h2> {evento.title} </h2>
-                        <h4> {description} </h4>
-                    </div>
-                </div>
-                <div className="containerBotones">
-                    <Link to={"/"}> Saber mas </Link>
-                    <Link to={"/"}>
-                        <div className="buttonComprar">
-                            <span>Comprar ahora</span>
-                        </div>
-                    </Link>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className='cardEvento'>
+			<img className='cardImagen' src={evento.image} alt={evento.title} />
+			<div className='infoEvento'>
+				<div className='containerInfo'>
+					<div className='fechaHoraContainer'>
+						<h3 className='fecha'> {fecha[2]} </h3>
+						<div className='mesHora'>
+							<h3>
+								{' '}
+								{meses[fecha[1]]}, {fecha[0]}{' '}
+							</h3>
+							<h4> {horario} </h4>
+						</div>
+					</div>
+					<div className='eventoArtista'>
+						<h2> {evento.title} </h2>
+						<h4> {description} </h4>
+					</div>
+				</div>
+				<div className='containerBotones'>
+					<Link to={'/'}> Saber mas </Link>
+					<Link to={'/'}>
+						<div className='buttonComprar'>
+							<span>Comprar ahora</span>
+						</div>
+					</Link>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default CardEvent;
+export default CardEvent
--- Front/src/pages/home/Home.jsx
+++ Front/src/pages/home/Home.jsx
@@ -1,30 +1,32 @@
-import { useEffect, useState } from "react";
-import SeccionEvent from "../../components/seccionEvents/SeccionEvent";
-import Newsletter from "../../components/Newsletter/Newsletter"
-import Footer from "../../components/footer/Footer"
-import Comments from "../../components/Comments/Comments";
+import { useEffect, useState } from 'react'
+import SeccionEvent from '../../components/seccionEvents/SeccionEvent'
+import Newsletter from '../../components/Newsletter/Newsletter'
+import Footer from '../../components/footer/Footer'
+import Comments from '../../components/Comments/Comments'
+import Carousel from '../../components/Carousel/Carousel'
 
 const Home = () => {
-
 	const [events, setEvents] = useState([])
 
-    const eventsFetch = async () => {
-        const response = await fetch("https://635eb27203d2d4d47af47b8b.mockapi.io/Cohorte")
-        const data = await response.json()
-        setEvents([data[0], data[1], data[2]])
-    }
+	const eventsFetch = async () => {
+		const response = await fetch(
+			'https://635eb27203d2d4d47af47b8b.mockapi.io/Cohorte'
+		)
+		const data = await response.json()
+		setEvents([data[0], data[1], data[2]])
+	}
 
-    useEffect(()=> {
-        eventsFetch()
-    }, [])
-
+	useEffect(() => {
+		eventsFetch()
+	}, [])
 
 	return (
 		<div>
-			<SeccionEvent seccion={"Proximos eventos"} eventos={events}/>
-            <Comments/>
-            <Newsletter/>
-            <Footer />
+			<Carousel />
+			<SeccionEvent seccion={'Proximos eventos'} eventos={events} />
+			<Comments />
+			<Newsletter />
+			<Footer />
 		</div>
 	)
 }
