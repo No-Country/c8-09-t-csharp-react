23bb3f00b6a5aac893de230598626d0b13031173
Alex Hnatiuk <alex.g.hnatiuk@gmail.com>
2022-11-17 02:51:45 -0300

    footer creado

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,12 +2,18 @@ import './App.css'
 import {Routes, Route} from "react-router-dom"
 import Home from './pages/home/Home'
 
+
+
 function App() {
 
 	return (
 		<Routes>
 			<Route element={<Home/>} path={"/"}/>
+			
+			
 		</Routes>
+
+		
 	)
 }
 
--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import Nav from './components/nav/Nav'
+import Footer from './components/footer/Footer'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
 		<React.StrictMode>
 			<Nav />
 			<App />
+			<Footer />
 		</React.StrictMode>
 	</BrowserRouter>
 )
