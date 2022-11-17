4f5e6e691f5342d9b0d3853272ad0e942b3cd16c
DnTo <danto@hotmail.com>
2022-11-17 16:46:20 -0500

    Create Endpoints for subscribing to newsletter

---
+++ Back/Core/Models/Newsletter/SubscribeDTO.cs
@@ -0,0 +1,12 @@
+﻿using DataAnnotationsExtensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace CohorteApi.Core.Models.Newsletter
+{
+    public class SubscribeDTO
+    {
+        [Email]
+        [Required]
+        public string Email { get; set; }
+    }
+}
---
+++ Back/Models/Subscription.cs
@@ -0,0 +1,16 @@
+﻿using DataAnnotationsExtensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace CohorteApi.Models
+{
+    public class Subscription
+    {
+        [Key]
+        public int Id { get; set; }
+        [Email]
+        [Required]
+        public string Email { get; set; }
+        public DateTime DateSubscribed { get; set ; }
+        public bool IsActive { get; set; } = true;
+    }
+}
