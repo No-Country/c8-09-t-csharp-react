a34c3fa15b73a05839383c86cfd9e5de28d47de9
kawpls <58341472+kawpls@users.noreply.github.com>
2022-11-17 19:11:55 -0600

    Update main.jsx

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
+import App from './app'
 import { BrowserRouter } from 'react-router-dom'
-import Nav from './components/nav/Nav'
+import Nav from './components/nav/nav'
 import { Provider } from 'react-redux'
-import {store} from './redux/store/Store'
+import {store} from './redux/store/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
