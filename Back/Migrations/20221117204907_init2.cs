b5eb4c5c0a787514a9d0084aec1256df5a7849e4
kawpls <58341472+kawpls@users.noreply.github.com>
2022-11-17 19:32:26 -0600

    Update and rename 20221117204907_init.cs to 20221117204907_init2.cs

--- Back/Migrations/20221117204907_init2.cs
+++ Back/Migrations/20221117204907_init2.cs
@@ -6,7 +6,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CohorteApi.Migrations
 {
-    public partial class init : Migration
+    public partial class init2 : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
