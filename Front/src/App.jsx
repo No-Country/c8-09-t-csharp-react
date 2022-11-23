3abbc81e7091de6e3d02c52fef2c33ce42d7a209
Alex Hnatiuk <alex.g.hnatiuk@gmail.com>
2022-11-22 22:00:13 -0300

    pagina 404 test

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -5,6 +5,8 @@ import Home from './pages/home/Home'
 import Login from './pages/login/login'
 import Register from './pages/register/register'
 import ForgotPassword from './pages/forgotPassword/forgotPassword'
+import Error from './pages/error/error'
+
 
 
 
@@ -16,6 +18,8 @@ function App() {
 			<Route element={<Login />} exact path={"/login"} />
 			<Route element={<Register />} path={"/register"} />
 			<Route element={<ForgotPassword />} path={"/forgotPassword"} />
+			<Route element={<Error />} path={"/error"} />
+
 		</Routes>
 
 		
