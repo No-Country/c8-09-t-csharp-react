2cf9a132dce1ecc247f1a45172e9cc2c7cd0e1b3
kawpls <58341472+kawpls@users.noreply.github.com>
2022-11-24 08:11:25 -0600

    Delete Front directory

--- Front/src/redux/actions/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export function createUser(payload){
-    return async function(dispatch){
-        const response = await axios.post("https://cohorteapi.azurewebsites.net/api/Authenticate/Register", payload)
-    }
-}
-
