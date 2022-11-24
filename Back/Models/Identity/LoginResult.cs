7d9274a2e893204b96d1af5666c9f59ae260e7e9
kawpls <58341472+kawpls@users.noreply.github.com>
2022-11-24 07:38:59 -0600

    Delete Back directory

--- Back/Models/Identity/LoginResult.cs
+++
@@ -1,7 +0,0 @@
-﻿public class LoginResult
-{
-    public bool Successful { get; set; }
-    public string Error { get; set; }
-    public string Token { get; set; }
-    public DateTime Expiration { get; set; }
-}
--- Back/Models/Identity/UserRoles.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CohorteApi.Models.Identity
-{
-    public static class UserRoles
-    {
-        public const string Admin = "Admin";
-        public const string User = "User";
-    }
-
-}
