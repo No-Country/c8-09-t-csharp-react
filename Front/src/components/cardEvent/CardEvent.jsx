22fd29661f807e06f5292e87bb2198575ccfa075
nasanchez7 <nadirblanco02@gmail.com>
2022-11-29 20:02:12 -0300

    actualizando estilos card

--- Front/src/components/cardEvent/CardEvent.jsx
+++ Front/src/components/cardEvent/CardEvent.jsx
@@ -17,14 +17,14 @@ const CardEvent = ({ evento }) => {
 		'Nov',
 		'Dic',
 	]
-	const description = evento.eventDescription.slice(0, 20)
+	//const description = evento.eventDescription.slice(0, 20)
 	const fechaSinFormatear = evento.eventTime.slice(0, 10)
 	const fecha = fechaSinFormatear.split('-')
 	const horario = evento.eventTime.slice(11, 16)
 
 	return (
 		<div className='cardEvento'>
-			<img className='cardImagen' src={evento.frontPageImage} alt={evento.eventName} />
+			<div className='cardImg' style={{backgroundImage: `url(${evento.thumbnail})`}}></div>
 			<div className='infoEvento'>
 				<div className='containerInfo'>
 					<div className='fechaHoraContainer'>
@@ -39,7 +39,7 @@ const CardEvent = ({ evento }) => {
 					</div>
 					<div className='eventoArtista'>
 						<h2> {evento.eventName} </h2>
-						<h4> {description} </h4>
+						<h4> {evento.eventDescription} </h4>
 					</div>
 				</div>
 				<div className='containerBotones'>
--- Front/src/components/nav/nav.jsx
+++ Front/src/components/nav/nav.jsx
@@ -69,7 +69,7 @@ const Nav = () => {
                 {isLogged ? 
                 <Menu as="div" className="relative inline-block text-left">
                     <div>
-                        <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                        <Menu.Button className="inline-flex w-full justify-center rounded-2xl bg-pink-400 px-4 py-2 text-sm font-medium text-white-700 shadow-sm hover:bg-pink-300">
                         {userName}
                         <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
                         </Menu.Button>
