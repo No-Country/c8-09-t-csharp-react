38ad1b3438131c79f1d20801e4c9236dde0225ab
DnTo <danto@hotmail.com>
2022-12-08 03:54:54 -0500

    Try1PRRestoreBack

--- Front/src/guards/AuthGuard.jsx
+++
@@ -1,5 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-
-export const AuthGuard = ({ redirectPath = '/login', isAllowed }) => {
-	return !isAllowed ? <Navigate to={redirectPath} replace /> : <Outlet />
-}
