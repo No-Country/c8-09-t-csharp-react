2437488c6c23c5f3cdfe5d5f02cd545b0c48374e
Maximiliano <maximartins45@gmail.com>
2022-12-17 20:27:37 -0300

    Update App.jsx

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 
-import Home from './pages/home/Home'
+import Home from './pages/home/home'
 import Login from './pages/login/login'
 import Register from './pages/register/register'
 import ForgotPassword from './pages/forgotPassword/forgotPassword'
@@ -13,13 +13,15 @@ import { isUserAdmin, isUserLogged } from './utils/validations'
 import Error from "./pages/error/error"
 import Event from './pages/event/event'
 import Catalogo from './pages/catalogo/catalogo'
+import Checkout from './components/Checkout/Checkout'
 
 function App() {
 
 	return (
 		<Routes>
 			<Route index element={<Home />} />
-			<Route element={<PrePurchase />} exact path={'/prePurchase'} />
+			<Route element={<PrePurchase />} exact path={'/prePurchase/'} />
+			<Route element={<Checkout />} exact path={'/checkout'}/>
 			<Route element={<ForgotPassword />} path={'/forgotPassword'} />
 			<Route element={<ResetPassword />} exact path={'/resetpassword'} />
 			<Route element={<Event />} path={'/event/:id'} />
